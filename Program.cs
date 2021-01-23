4815438db1ac0382c273f5b7961062621ca1efd9
bu <13253495869@163.com>
2021-01-23 10:56:19 +0800

    1

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = DownloadPicture();
+            changeImg(path);
+            //Console.ReadLine();
+            //File.Delete(path);
+        }
+
+        public static string DownloadPicture(int timeOut = 10000)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
+
+            string picUrl = "https://huuu.xyz/php/bing2.php";
+            string savePath = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMdd") + ".jpg";
+            string value = String.Empty;
+            WebResponse response = null;
+            Stream stream = null;
+            try
+            {
+                WebClient mywebclient = new WebClient();
+                mywebclient.DownloadFile(picUrl, savePath);
+                value = savePath;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (stream != null) stream.Close();
+                if (response != null) response.Close();
+            }
+            return value;
+        }
+
+        public class WinAPI
+        {
+            [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+            public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        }
+
+        private static void changeImg(string bmpPath)
+        {
+            int nResult;
+            if (File.Exists(bmpPath))
+            {
+
+                nResult = WinAPI.SystemParametersInfo(20, 1, bmpPath, 0x1 | 0x2); //更换壁纸
+                if (nResult == 0)
+                {
+                    Console.WriteLine("err");
+                }
+                else
+                {
+                    RegistryKey hk = Registry.CurrentUser;
+                    RegistryKey run = hk.CreateSubKey(@"Control Panel\Desktop\");
+                    run.SetValue("Wallpaper", bmpPath);  //将新图片路径写入注册表
+                    Console.WriteLine("success");
+                }
+            }
+            else
+            {
+                Console.WriteLine("not exist");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿C#更换壁纸 https://www.cnblogs.com/xuchi/archive/2011/12/26/2302416.html
+C#下载图片 https://www.cnblogs.com/loushengjie/p/10734257.html
+tls		   http://www.voidcn.com/article/p-qctsmyjp-bys.html
